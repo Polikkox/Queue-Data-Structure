b4ab2eb92ba6d1f4a355536d2e59488085a54ebe
Polikkox <damian.targosz11@gmail.com>
2019-02-10 19:51:41 +0100

    Add methods enqueue and peek

--- src/main/java/Queue.java
+++ src/main/java/Queue.java
@@ -1,2 +1,28 @@
-public class Queue {
+
+
+public class Queue <E> {
+
+    private Node head;
+    private Node tail;
+
+    @SuppressWarnings({"unchecked"})
+    public void enqueue (E value){
+        if(head == null){
+            this.head = new Node(value);
+            this.tail = head;
+        }
+        else{
+            while (this.tail.getNextNode() != null){
+                this.tail = this.tail.getNextNode();
+            }
+            this.tail.setNextNode(new Node(value));
+        }
+
+    }
+
+
+
+    public Object peek(){
+        return this.head.getValue();
+    }
 }
