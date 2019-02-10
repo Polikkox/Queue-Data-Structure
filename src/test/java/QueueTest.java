91a819c0363961c4dc484c6283f70bd3e0ed7d53
Polikkox <damian.targosz11@gmail.com>
2019-02-10 19:51:54 +0100

    Add tests to methods enqueue and peek

---
+++ src/test/java/QueueTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueueTest {
+
+    private Queue queue;
+
+    @BeforeEach
+    public void createInstanceOfClass(){
+        this.queue = new Queue();
+
+        assertNotNull(queue);
+    }
+
+    @Test
+    public void testAddOneItemToQueue(){
+        String value = "test";
+
+        this.queue.enqueue(value);
+        assertEquals(value, this.queue.peek());
+    }
+
+    @Test
+    public void testAddThreeItemsToQueue(){
+        String value = "test";
+        String value2 = "test2";
+        String value3 = "test3";
+
+        this.queue.enqueue(value);
+        this.queue.enqueue(value2);
+        this.queue.enqueue(value3);
+        assertEquals(value, this.queue.peek());
+    }
+}
