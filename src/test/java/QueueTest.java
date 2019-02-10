bc1e0d95d273150d243eff3f11c04abf948b7f6b
Polikkox <damian.targosz11@gmail.com>
2019-02-10 19:58:31 +0100

    Add test to method dequeue

--- src/test/java/QueueTest.java
+++ src/test/java/QueueTest.java
@@ -33,4 +33,24 @@ class QueueTest {
         this.queue.enqueue(value3);
         assertEquals(value, this.queue.peek());
     }
+
+    @Test
+    public void testDequeueOneElement(){
+        String value = "test";
+        this.queue.enqueue(value);
+        assertEquals(value, this.queue.dequeue());
+    }
+
+    @Test
+    public void testDequeueThreeElement(){
+        String value = "test";
+        String value2 = "test2";
+        String value3 = "test3";
+        this.queue.enqueue(value);
+        this.queue.enqueue(value2);
+        this.queue.enqueue(value3);
+        assertEquals(value, this.queue.dequeue());
+        assertEquals(value2, this.queue.dequeue());
+        assertEquals(value3, this.queue.dequeue());
+    }
 }
